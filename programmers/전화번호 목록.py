31671dcf7e567de8a2612d222d06a01f28ef8dda
eunji <57802437+gogodit@users.noreply.github.com>
2021-04-20 20:48:54 +0900

    Update 전화번호 목록.py

--- programmers/전화번호 목록.py
+++ programmers/전화번호 목록.py
@@ -2,7 +2,8 @@
 # 다음에 다시...
 def solution(phone_book):
     phone_book.sort()
-    for p1, p2 in zip(phone_book, phone_book[1:]):
-        if p2.startswith(p1):
+    for p1, p2 in zip(phone_book, phone_book[1:]): 
+        # zip함수에 길이가 같은 리스트를 넣어 병렬 비교 및 반환
+        if p2.startswith(p1):  # startswith(시작하는문자, 시작지점)
             return False
     return True
