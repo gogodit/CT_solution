707bcd398a261c4a9042c78650e17791748e0b0b
eunji <57802437+gogodit@users.noreply.github.com>
2021-06-24 22:36:48 +0900

    Create 상위 n개 레코드.sql

---
+++ programmers/MySQL/상위 n개 레코드.sql
@@ -0,0 +1,10 @@
+/* https://programmers.co.kr/learn/courses/30/lessons/59405 */
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1
+
+/* 
+limit : 레코드를 순서대로 표시할 때 표시하 레코드의 수 지정 
+offset : 범위 지정
+*/
